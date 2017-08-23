c3a5f332168afeb730d520a2d1dcae2948b2c0a9
WuNL <wunailiang@gmail.com>
2017-08-23 23:09:35 +0800

    add textrender class, english work fine

---
+++ include/textRender.h
@@ -0,0 +1,40 @@
+#ifndef TEXTRENDER_H
+#define TEXTRENDER_H
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+// FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H
+// GL includes
+#include "Shader.h"
+
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character
+{
+    GLuint TextureID;   // ID handle of the glyph texture
+    glm::ivec2 Size;    // Size of glyph
+    glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+    GLuint Advance;    // Horizontal offset to advance to next glyph
+};
+
+
+class textRender
+{
+public:
+    textRender(int window_width, int window_height, const char* path);
+    virtual ~textRender();
+    void RenderText(std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
+    void fillChar(char c, Character& ch);
+protected:
+
+private:
+    Shader shader;
+    FT_Library ft;
+    FT_Face face;
+
+    GLuint VAO, VBO;
+};
+
+#endif // TEXTRENDER_H
